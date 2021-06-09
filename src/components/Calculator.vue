ad5a2c4448b7303a80eaf2611280002056833dcf
mdtahidulislam <tahidul.developer@gmail.com>
2021-06-09 17:57:52 +0600

    calculation done

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,25 +1,26 @@
 <template>
   <div class="calculator">
-    <div class="btn display">{{ current || '0' }}</div>
+    <div class="btn display">{{ answer }}</div>
+    <div class="btn display">{{ logList + current  }}</div>
     <div class="btn" @click="clear">C</div>
     <div class="btn" @click="sign">+/-</div>
     <div class="btn" @click="percent">%</div>
-    <div class="btn operator">/</div>
+    <div class="btn operator" @click="divide">/</div>
     <div class="btn" @click="append(7)">7</div>
     <div class="btn" @click="append(8)">8</div>
     <div class="btn" @click="append(9)">9</div>
-    <div class="btn operator">x</div>
+    <div class="btn operator" @click="multiplication">x</div>
     <div class="btn" @click="append(4)">4</div>
     <div class="btn" @click="append(5)">5</div>
     <div class="btn" @click="append(6)">6</div>
-    <div class="btn operator">-</div>
+    <div class="btn operator" @click="minus">-</div>
     <div class="btn" @click="append(1)">1</div>
     <div class="btn" @click="append(2)">2</div>
     <div class="btn" @click="append(3)">3</div>
-    <div class="btn operator">+</div>
+    <div class="btn operator" @click="add">+</div>
     <div class="btn zero" @click="append(0)">0</div>
     <div class="btn" @click="dot">.</div>
-    <div class="btn operator">=</div>
+    <div class="btn operator" @click="equal">=</div>
   </div>
 </template>
 
@@ -28,12 +29,17 @@ export default {
   name: 'Calculator',
   data() {
     return {
-      current: '123',
+      answer: '',
+      logList: '',
+      current: '',
+      operatorClicked: true,
     }
   },
   methods: {
     clear () {
       this.current = '';
+      this.logList = '';
+      this.answer = '';
     },
     sign () {
       this.current = this.current.charAt(0) === '-' ? this.current.slice(1) : `-${this.current}`;
@@ -44,6 +50,10 @@ export default {
 
     // input number
     append(number) {
+      if ( this.operatorClicked ) {
+        this.current = '';
+        this.operatorClicked = false;
+      }
       this.current = `${this.current}${number}`;
     },
 
@@ -52,6 +62,37 @@ export default {
       if(this.current.indexOf('.') === -1) {
         this.append('.');
       }
+    },
+
+    addtoLog(operator) {
+      if ( this.operatorClicked == false ) {
+        this.logList += `${this.current} ${operator} `;
+        this.current = '';
+        this.operatorClicked = true;
+      }
+    },
+    // add something
+    add () {
+      this.addtoLog('+');
+    },
+    minus () {
+      this.addtoLog('-');
+    },
+    multiplication () {
+      this.addtoLog('*');
+    },
+    divide () {
+      this.addtoLog('/');
+    },
+
+    // result show
+    equal () {
+      if ( this.operatorClicked === false) {
+        this.answer = eval(this.logList + this.current);
+      } else {
+        this.answer = "WHAT?!!";
+      }
+      
     }
   }
   
@@ -83,6 +124,7 @@ export default {
 .btn {
   background-color: #f2f2f2;
   border: 1px solid #999;
+  cursor: pointer;
 }
 
 .operator {
